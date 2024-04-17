a401aac5b9433384823d82affba98271fdd997b5
Arka Ghosh <71174892+aghoshpro@users.noreply.github.com>
2024-04-17 21:31:25 +0700

    Update import-data.sh with shp2pgsql commands

--- rasdatabase/import-data.sh
+++ rasdatabase/import-data.sh
@@ -2,3 +2,6 @@
 
 # shp2pgsql scripts
 #TODO: Add scripts
+shp2pgsql -s 4326 /path_to_shapefile/Bavaria_mun.shp region_Bavaria | psql -h localhost -p 5432 -U postgres -d VectorDB
+shp2pgsql -s 4326 /path_to_shapefile/Sweden_mun.shp region_Sweden | psql -h localhost -p 5432 -U postgres -d VectorDB
+shp2pgsql -s 4326 /path_to_shapefile/South_Tyrol_LOD3.shp region_South_Tyrol | psql -h localhost -p 5432 -U postgres -d VectorDB
