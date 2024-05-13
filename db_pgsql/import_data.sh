7aa8e5e1145a9d77ebbb67b78338e99eb34fe72a
Arka Ghosh <71174892+aghoshpro@users.noreply.github.com>
2024-05-13 20:20:31 +0700

    Update import_data.sh

--- db_pgsql/import_data.sh
+++ db_pgsql/import_data.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Vector Geometry Data of REGION_OF_INTEREST(ROI)
-shp2pgsql -s 4326 /home/arkaghosh/Downloads/Bolzano/Vector/South_Tyrol_LOD3.shp region_South_Tyrol | psql -h localhost -p 5432 -U postgres -d VectorDB
-shp2pgsql -s 4326 /home/arkaghosh/Downloads/Baveria/Vector/Baveria_1.shp region_bavaria | psql -h localhost -p 5432 -U postgres -d VectorDB
-shp2pgsql -s 4326 /home/arkaghosh/Downloads/Sweden/Vector/SWE_adm2.shp region_sweden | psql -h localhost -p 5432 -U postgres -d VectorDB
+shp2pgsql -s 4326 /db_pgsql/vector_data/Bavaria_1.shp region_Bavaria | psql -h localhost -p 5432 -U postgres -d VectorDB
+shp2pgsql -s 4326 /db_pgsql/vector_data/gadm41_SWE_2.shp region_Sweden | psql -h localhost -p 5432 -U postgres -d VectorDB
+shp2pgsql -s 4326 /db_pgsql/vector_data/South_Tyrol_LOD3.shp region_South_Tyrol | psql -h localhost -p 5432 -U postgres -d VectorDB
 # Add more shp2pgsql commands as needed...
 
 # TODO: Check if port is correct or MAPPED_PORT is needed
