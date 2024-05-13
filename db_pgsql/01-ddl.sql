8bb0eb4a22e49dc32336eaebba42cb8811f770fc
Arka Ghosh <71174892+aghoshpro@users.noreply.github.com>
2024-05-13 20:33:42 +0700

    Update 01-ddl.sql

--- db_pgsql/01-ddl.sql
+++ db_pgsql/01-ddl.sql
@@ -18,67 +18,171 @@ CREATE DATABASE "VectorDB"
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
 
--- **********************************************************************************************************************************************
--- Table: public.petascopedb01 (holds metadata for each raster stored in rasdaman)
 
--- DROP TABLE IF EXISTS public.petascopedb01;
+-- ###########################################
+-- ### LookUp Table Creation In petacopedb ### **********************************************************************************************************************************************
+-- ###########################################
 
-CREATE TABLE IF NOT EXISTS public.petascopedb01
-(
-    raster_id bigint NOT NULL,
-    raster_name character varying COLLATE pg_catalog."default",
-    axis_label character varying COLLATE pg_catalog."default",
-    axis_type character varying COLLATE pg_catalog."default",
-    dom_lower_bound character varying COLLATE pg_catalog."default",
-    grid_lower_bound bigint,
-    dom_upper_bound character varying COLLATE pg_catalog."default",
-    grid_upper_bound bigint,
-    resolution double precision,
-    scale_factor double precision,
-    CONSTRAINT petascopedb01_pkey PRIMARY KEY (raster_id)
-)
+-- 1. Selected Tables from petascopedb
 
-TABLESPACE pg_default;
+SELECT * FROM public.axis_extent
 
-ALTER TABLE IF EXISTS public.petascopedb01
-    OWNER to postgres;
+SELECT * FROM public.geo_axis
 
--- **********************************************************************************************************************************************
+SELECT * FROM public.index_axis
 
--- FUNCTION: rasdaman_op.check_rasdaman_conn()
+SELECT * FROM public.coverage
 
--- DROP FUNCTION IF EXISTS rasdaman_op.check_rasdaman_conn();
+SELECT * FROM public.envelope
 
-CREATE OR REPLACE FUNCTION rasdaman_op.check_rasdaman_conn(
-	OUT db_connector text)
-    RETURNS text
-    LANGUAGE 'plpython3u'
+SELECT * FROM public.envelope_by_axis
+
+SELECT * FROM public.wgs84_bounding_box
+	
+
+-- 2. lookup_temp (to get extent as double)
+CREATE OR REPLACE VIEW lookup_temp_X AS
+	SELECT coverage.id, coverage.coverage_id, wgs84_bounding_box.max_lat, wgs84_bounding_box.min_lat, wgs84_bounding_box.max_long, wgs84_bounding_box.min_long 	
+	FROM public.coverage
+	JOIN public.envelope ON coverage.envelope_id = envelope.envelope_id
+	JOIN public.envelope_by_axis ON  envelope.envelope_by_axis_id = envelope_by_axis.envelope_by_axis_id
+	JOIN public.wgs84_bounding_box ON envelope_by_axis.wgs84_bounding_box_id = wgs84_bounding_box.wgs84_bounding_box_id
+		
+
+-- 3. lookup_peta build from selected tables joined lookup_temp.
+	
+CREATE OR REPLACE VIEW lookup_peta_X AS	
+	SELECT coverage.id, coverage.coverage_id,  axis_extent.axis_label, axis_extent.lower_bound, axis_extent.grid_lower_bound, axis_extent.upper_bound, axis_extent.grid_upper_bound,lookup_temp_X.max_lat, lookup_temp_X.min_lat, lookup_temp_X.max_long, lookup_temp_X.min_long, geo_axis.resolution
+	FROM public.coverage, public.envelope,  public.axis_extent, public.geo_axis, lookup_temp_X
+	WHERE coverage.envelope_id = envelope.envelope_id 
+	AND envelope.envelope_by_axis_id = axis_extent.envelope_by_axis_id 
+	AND axis_extent.upper_bound = geo_axis.upper_bound
+	AND coverage.id = lookup_temp_X.id
+
+    -- OR
+
+CREATE OR REPLACE VIEW lookup_peta_X AS	
+    SELECT coverage.id, geo_axis.general_grid_id, coverage.coverage_id,  axis_extent.axis_label, axis_extent.axis_type, axis_extent.uom_label AS unit, axis_extent.lower_bound, axis_extent.grid_lower_bound, axis_extent.upper_bound, axis_extent.grid_upper_bound, geo_axis.resolution
+    FROM public.coverage, public.envelope,  public.axis_extent, public.geo_axis
+    WHERE coverage.envelope_id = envelope.envelope_id 
+    AND envelope.envelope_by_axis_id = axis_extent.envelope_by_axis_id 
+    AND axis_extent.upper_bound = geo_axis.upper_bound
+
+SELECT * FROM lookup_peta_X -- Lookup Table in petascopedb
+
+-- 4. Switch to VectorDB and import lookup_peta_X as lookup_main_X using dblink
+
+CREATE OR REPLACE VIEW lookup_main_X1 AS		
+SELECT *
+    FROM dblink('host=localhost dbname=petascopedb user=petauser password=petapasswd options=-csearch_path=',
+	   'SELECT id, coverage_id, axis_label, lower_bound, upper_bound, grid_lower_bound, grid_upper_bound, resolution, min_long, max_long, min_lat, max_lat FROM public.lookup_peta_x')
+AS remote_table(raster_id text, raster_name text, axis_label text, domain_lower_bound text, domain_upper_bound date, grid_lower_bound date, grid_upper_bound integer, resolution float, min_long float, max_long float, min_lat float, max_lat float);
+
+
+
+-- 5. Build sample_lookup_X
+
+CREATE OR REPLACE VIEW sample_lookup_X1 AS
+SELECT 
+    raster_id,
+    raster_name,
+    MAX(CASE WHEN axis_label = 'Long' THEN min_long END) AS min_lon,
+    MAX(CASE WHEN axis_label = 'Long' THEN max_long END) AS max_lon,
+	MAX(CASE WHEN axis_label = 'Long' THEN grid_lower_bound END) AS min_lon_grid,
+    MAX(CASE WHEN axis_label = 'Long' THEN grid_upper_bound END) AS max_lon_grid,
+	MAX(CASE WHEN axis_label = 'Long' THEN resolution END) AS res_lon,
+    MAX(CASE WHEN axis_label = 'Lat' THEN min_lat END) AS min_lat,
+    MAX(CASE WHEN axis_label = 'Lat' THEN max_lat END) AS max_lat,
+	MAX(CASE WHEN axis_label = 'Lat' THEN grid_lower_bound END) AS min_lat_grid,
+    MAX(CASE WHEN axis_label = 'Lat' THEN grid_upper_bound END) AS max_lat_grid,
+	MAX(CASE WHEN axis_label = 'Lat' THEN resolution END) AS res_lat,
+	MAX(CASE WHEN axis_label = 'ansi' THEN domain_lower_bound date END) AS start_time,
+    MAX(CASE WHEN axis_label = 'ansi' THEN domain_upper_bound date END) AS end_time,
+	MAX(CASE WHEN axis_label = 'ansi' THEN grid_lower_bound END) AS start_time_grid,
+    MAX(CASE WHEN axis_label = 'ansi' THEN grid_upper_bound END) AS end_time_grid,
+	MAX(CASE WHEN axis_label = 'ansi' THEN resolution END) AS res_time
+FROM lookup_main_X1
+GROUP BY raster_id, raster_name;
+
+
+select * from sample_lookup_X1 -- Build mappings with this table
+	
+
+-- ###########################################
+-- ### PL/Pgsql Functions ### **********************************************************************************************************************************************
+-- ###########################################
+
+-- FUNCTION 01: rasdaman_op.timestamp2grid(text, text)
+
+-- DROP FUNCTION IF EXISTS rasdaman_op.timestamp2grid(text, text);
+
+CREATE OR REPLACE FUNCTION rasdaman_op.timestamp2grid(
+	input_time text,
+	input_raster text)
+    RETURNS integer
+    LANGUAGE 'plpgsql'
     COST 100
     VOLATILE PARALLEL UNSAFE
 AS $BODY$
-from rasdapy.db_connector import DBConnector
-from rasdapy.query_executor import QueryExecutor
+DECLARE
+    example_date_grid_val INTEGER;
+BEGIN
+    SELECT start_time_grid + EXTRACT(DAY FROM (input_time::TIMESTAMP - start_time::TIMESTAMP))INTO example_date_grid_val
+    FROM sample_lookup_X1
+    WHERE start_time::TIMESTAMP <= input_time::TIMESTAMP AND end_time::TIMESTAMP >= input_time::TIMESTAMP AND raster_name = input_raster;
+    
+    RETURN example_date_grid_val;
+END;
 
-db_connector = DBConnector("localhost", 7001, "rasadmin", "rasadmin")
-query_executor = QueryExecutor(db_connector)
-
-db_connector.open() # connection open
-
-if bool(db_connector) == True: 
-	return 'RasDaMan is connected'
-
-db_connector.close()
 $BODY$;
 
-ALTER FUNCTION rasdaman_op.check_rasdaman_conn()
+ALTER FUNCTION rasdaman_op.timestamp2grid(text, text)
     OWNER TO postgres;
 
--- FUNCTION: rasdaman_op.geo2grid_coords(text)
+-- FUNCTION 02: rasdaman_op.get_geom_wkt(geometry)
 
--- DROP FUNCTION IF EXISTS rasdaman_op.geo2grid_coords(text);
+-- DROP FUNCTION IF EXISTS rasdaman_op.get_geom_wkt(geometry);
 
-CREATE OR REPLACE FUNCTION rasdaman_op.geo2grid_coords(
+CREATE OR REPLACE FUNCTION rasdaman_op.get_geom_wkt(
+	geometry_wkt geometry)
+    RETURNS text
+    LANGUAGE 'plpgsql'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+AS $BODY$
+DECLARE
+    out_geom_wkt TEXT;
+BEGIN
+	SELECT 
+	    CASE 
+	        WHEN ST_NumGeometries(geometry_wkt) = 1 THEN ST_AsText(ST_GeometryN(geometry_wkt, 1))
+	        ELSE ST_AsText(geometry_wkt)
+	    END AS geometry_wkt INTO out_geom_wkt;
+
+	RETURN out_geom_wkt;
+END;
+
+$BODY$;
+
+ALTER FUNCTION rasdaman_op.get_geom_wkt(geometry)
+    OWNER TO postgres;
+
+
+-- ###########################################
+-- ### PL/Python ### **********************************************************************************************************************************************
+-- ###########################################
+
+
+-- FUNCTION: rasdaman_op.geo2grid_final(text, double precision, double precision, double precision, double precision)
+
+-- DROP FUNCTION IF EXISTS rasdaman_op.geo2grid_final(text, double precision, double precision, double precision, double precision);
+
+CREATE OR REPLACE FUNCTION rasdaman_op.geo2grid_final(
 	"geoPOLY" text,
+	min_lon double precision,
+	max_lat double precision,
+	resolution_lon double precision,
+	resolution_lat double precision,
 	OUT "gridPOLY" text)
     RETURNS text
     LANGUAGE 'plpython3u'
@@ -90,20 +194,16 @@ import re
 import gdal
 from affine import Affine
 from shapely.geometry import Polygon
+from shapely.ops import unary_union
 from shapely import wkt
-
--- Required metadata for geo2grid translation. different for each raster  
-xmin = 8.979166665862266  
-ymax = 50.979166665862266 
-pixel_size = 0.008333333332587
 
 def grid2WKT_polygon(y_grid, x_grid):
     coordinates = list(zip(y_grid, x_grid))
     polygon = "POLYGON((" + ", ".join(f"{x} {y}" for x, y in coordinates) + "))"
     return polygon
 
-def geo2grid(lons, lats, xmin, ymax, pixel_size, xskew = 0.0, yskew = 0.0):
-    aff_gdal = Affine.from_gdal(xmin, pixel_size, xskew, ymax, 0.0, -pixel_size)
+def geo2grid(lons, lats, xmin, ymax, x_scale, y_scale, xskew = 0.0, yskew = 0.0):
+    aff_gdal = Affine.from_gdal(xmin, x_scale, xskew, ymax, 0.0, -y_scale)
     lons = np.array(lons)
     lats = np.array(lats)
     xs, ys = ~aff_gdal*(lons, lats)
@@ -117,8 +217,8 @@ def add_closing_coordinates(d):
     c = d.index(',')    
     return d[:j] + ", " + d[i:c] + d[j:]
 
-def processPOLYGON(inputPOLYGON, regionID = None):
-    if inputPOLYGON.area < pixel_size:
+def processPOLYGON(inputPOLYGON):
+    if inputPOLYGON.area < x_scale:
 	    pass
     elif len(inputPOLYGON.interiors) == 0:
         coords = np.dstack(inputPOLYGON.boundary.xy).tolist()[0][:-1]
@@ -132,52 +232,59 @@ def processPOLYGON(inputPOLYGON, regionID = None):
         long_list = long_arr.tolist()
         lat_list = lat_arr.tolist()
 
-        x_grid, y_grid = geo2grid(long_list, lat_list, xmin, ymax, pixel_size)
+        x_grid, y_grid = geo2grid(long_list, lat_list, xmin, ymax, x_scale, y_scale)
         gridPOLYGON_yx = grid2WKT_polygon(y_grid, x_grid)
 
         return gridPOLYGON_yx
     else:
-        print(f"{regionID}: Polygon Ring Detected")
         mainPOLYGON = Polygon(inputPOLYGON.exterior)  
         gridPOLYGON_yx = processPOLYGON(mainPOLYGON)
 
         return gridPOLYGON_yx
 
-           
-def geoPOLYGON_to_gridPOLYGON_03(inputREGION, regionID = None):
+def geoPOLYGON_to_gridPOLYGON_03(inputREGION, min_lon, max_lat, resolution_lon, resolution_lat):
     polygons_array1 = []
     polygons_array2 = []
     i = 0
     r = 0
+    global xmin
+    xmin = min_lon
+    global ymax
+    ymax= max_lat
+    global x_scale
+    x_scale = resolution_lon
+    global y_scale
+    y_scale = resolution_lat
+	
     inputREGION = wkt.loads(inputREGION)
     if inputREGION.geom_type == 'Polygon':
-        return processPOLYGON(inputREGION, regionID)
+        return processPOLYGON(inputREGION)
     
     elif len(list(inputREGION.geoms[0].interiors)) > 0:         
         for polygon in inputREGION.geoms:
             gridPOLYGON = processPOLYGON(polygon)
             polygons_array1.append(gridPOLYGON)
-            r = r +1
             
-        gridMULTI =  [shapely.wkt.loads(poly) for poly in polygons_array1]
+        gridMULTI =  [wkt.loads(poly) for poly in polygons_array1]
         return shapely.geometry.MultiPolygon(gridMULTI)
 
     else:                                                  
-        print(f"{regionID}: MultiPolygon is processing")
         for polygon in inputREGION.geoms:
             gridPOLYGON = processPOLYGON(polygon)
             polygons_array2.append(gridPOLYGON)
-            i = i +1
             
-        gridMULTI =  [shapely.wkt.loads(poly) for poly in polygons_array2]
-        return shapely.geometry.MultiPolygon(gridMULTI)
-
-gridPOLY = geoPOLYGON_to_gridPOLYGON_03(geoPOLY)
+        gridMULTI =  [wkt.loads(poly) for poly in polygons_array2]
+        return unary_union(gridMULTI)
+	
+gridPOLY = geoPOLYGON_to_gridPOLYGON_03(geoPOLY, min_lon, max_lat, resolution_lon, resolution_lat)
 return gridPOLY
+	
+           
 $BODY$;
 
-ALTER FUNCTION rasdaman_op.geo2grid_coords(text)
+ALTER FUNCTION rasdaman_op.geo2grid_final(text, double precision, double precision, double precision, double precision)
     OWNER TO postgres;
+
 
 -- FUNCTION: rasdaman_op.query2array(text)
 
@@ -213,6 +320,8 @@ $BODY$;
 ALTER FUNCTION rasdaman_op.query2array(text)
     OWNER TO postgres;
 
+
+
 -- FUNCTION: rasdaman_op.query2numeric(text)
 
 -- DROP FUNCTION IF EXISTS rasdaman_op.query2numeric(text);
@@ -245,5 +354,39 @@ $BODY$;
 
 ALTER FUNCTION rasdaman_op.query2numeric(text)
     OWNER TO postgres;
+
+-- FUNCTION: rasdaman_op.query2string(text)
+
+-- DROP FUNCTION IF EXISTS rasdaman_op.query2string(text);
+
+CREATE OR REPLACE FUNCTION rasdaman_op.query2string(
+	query text,
+	OUT text)
+    RETURNS text
+    LANGUAGE 'plpython3u'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+AS $BODY$
+from rasdapy.db_connector import DBConnector
+from rasdapy.query_executor import QueryExecutor
+
+def query2result(query):
+   output_val = query_executor.execute_read(query)
+   return output_val
+	
+db_connector = DBConnector("localhost", 7001, "rasadmin", "rasadmin")
+query_executor = QueryExecutor(db_connector)
+db_connector.open()
+
+try:
+   return query2result(query)
+finally:
+   db_connector.close()
+$BODY$;
+
+ALTER FUNCTION rasdaman_op.query2string(text)
+    OWNER TO postgres;
+
+
 
 
