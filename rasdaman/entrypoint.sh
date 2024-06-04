cd6ca092aa5a11991a36d8e78d43f3a9304c52aa
Arka Ghosh <71174892+aghoshpro@users.noreply.github.com>
2024-06-04 15:52:31 +0200

    sweden.json is added to entrypoint.sh

--- rasdaman/entrypoint.sh
+++ rasdaman/entrypoint.sh
@@ -8,6 +8,7 @@ sleep 60
 echo "Run wcst_import.sh"
 /opt/rasdaman/bin/wcst_import.sh /data/Bavaria_Temp.json
 /opt/rasdaman/bin/wcst_import.sh /data/South_Tyrol_Temp.json
+/opt/rasdaman/bin/wcst_import.sh /data/Sweden_Temperature_1km.json
 
 # Keep the container running
 tail -f /dev/null
