5706c1ac033030667461b7b3ec8761424d1cdcdc
Arka Ghosh <71174892+aghoshpro@users.noreply.github.com>
2024-06-22 00:02:37 +0200

    Final changes

---
+++ diagrams/sparqlEndpoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sleep 12
+echo -e "\n"
+echo -e "░█▀█░░░░░░▄░░░░░░█▀▄░█▀█░█▀▀░▀█▀░█▀▀░█▀▄"
+echo -e "░█░█░█▀█░▀█▀░█▀█░█▀▄░█▀█░▀▀█░░█░░█▀▀░█▀▄"
+echo -e "░▀▀▀░▀░▀░░▀░░▀▀▀░▀░▀░▀░▀░▀▀▀░░▀░░▀▀▀░▀░▀ v1.0\n" 
+echo -e "Ontop SPARQL Endpoint is live at \e[5;4;92mhttp://localhost:8082/\e[0m\n"                                                                                                 
+                                                                                                         
+# Keep the container running
+tail -f /dev/null
