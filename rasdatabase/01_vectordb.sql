0469edbb5410d84c572cd2066bbc3e7953e919b4
aghoshpro <aghoshpro@gmail.com>
2024-12-27 17:39:08 +0100

    [#3b] - albulenpano/geosparql_1.1:v5 is added and queries are updated

--- rasdatabase/01_vectordb.sql
+++ rasdatabase/01_vectordb.sql
@@ -245,3 +245,101 @@ try:
 finally:
    db_connector.close()
 $BODY$;
+
+-- FUNCTION: rasdaman_op.query2geotiff(text, text, text, double precision)
+
+CREATE OR REPLACE FUNCTION rasdaman_op.query2geotiff(
+	query text,
+	regionWkt text,
+	filename text,
+	fill_val double precision,
+	OUT folium text)
+    RETURNS text
+    LANGUAGE 'plpython3u'
+    COST 100
+    VOLATILE PARALLEL UNSAFE
+AS $BODY$
+from rasdapy.db_connector import DBConnector
+from rasdapy.query_executor import QueryExecutor
+from shapely import Point, Polygon, bounds, wkt
+import numpy as np
+import os
+import gdal
+from osgeo import osr
+import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+import rasterio
+from rasterio.plot import show, reshape_as_image, reshape_as_raster, reshape_as_image
+import folium
+from folium.raster_layers import ImageOverlay
+
+def get_polygon_extent(polygon):
+    coords = list(polygon.exterior.coords)
+    xs = [x for x, y in coords]
+    ys = [y for x, y in coords]
+    return [min(xs), min(ys), max(xs), max(ys)]
+	
+def getGeoTransform(extent, nlines, ncols):
+    resx = (extent[2] - extent[0]) / ncols
+    resy = (extent[3] - extent[1]) / nlines
+    return [extent[0], resx, 0, extent[3] , 0, -resy]
+	
+def query2folium(query, regionWkt, filename, fill_val):
+    result = query_executor.execute_read(query) 
+    numpy_array = result.to_array()
+    if fill_val is not None:
+        numpy_array = numpy_array.astype('float')
+        numpy_array[numpy_array == fill_val] = 'nan'
+
+    driver = gdal.GetDriverByName('GTiff')  
+    nrows = numpy_array.shape[0]
+    ncols = numpy_array.shape[1]
+    data_type = gdal.GDT_Float32
+    grid_data = driver.Create('grid_data', ncols, nrows, 1, data_type)
+    grid_data.GetRasterBand(1).WriteArray(numpy_array)
+    srs = osr.SpatialReference()
+    srs.ImportFromProj4('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
+    grid_data.SetProjection(srs.ExportToWkt())
+    regionWkt = wkt.loads(regionWkt)
+    extent = get_polygon_extent(regionWkt)
+    grid_data.SetGeoTransform(getGeoTransform(extent, nrows, ncols))	
+    file_name = str(filename)+'.tif'
+    driver.CreateCopy(file_name, grid_data, 0)  
+    driver = None
+    grid_data = None          
+    os.remove('grid_data')
+    elevRaster = rasterio.open(file_name)
+    elevArray = elevRaster.read(1)
+    boundList = [x for x in elevRaster.bounds]
+    norm = Normalize(vmin=np.nanmin(elevArray), vmax=np.nanmax(elevArray))
+    cmap = plt.cm.get_cmap('jet')
+    def colormap_function(x):
+        rgba = cmap(norm(x))
+        return (rgba[0], rgba[1], rgba[2], rgba[3])
+
+    rasLon = (boundList[3] + boundList[1])/2
+    rasLat = (boundList[2] + boundList[0])/2
+    mapCenter = [rasLon, rasLat]
+
+    mf = folium.Map(location=mapCenter, zoom_start=13)
+    folium.raster_layers.ImageOverlay(
+        image=elevArray,
+        bounds=[[boundList[1], boundList[0]], [boundList[3], boundList[2]]],
+        opacity=0.52,
+        colormap=colormap_function,  
+		interactive=True,
+        cross_origin=False,
+    ).add_to(mf)
+    
+    return mf
+
+db_connector = DBConnector("localhost", 7001, "rasadmin", "rasadmin")
+query_executor = QueryExecutor(db_connector)
+db_connector.open()
+
+try:
+   folium= query2folium(query, regionWkt, None, None)
+   return folium
+finally:
+   db_connector.close()	
+$BODY$;
